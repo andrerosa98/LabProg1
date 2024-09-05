f6bc6cdf3d1814f0cf4419e400ae15b299a7381f
andrerosa98 <20242012000024@pucgo.edu.br>
2024-09-05 09:17:15 -0300

    feito o exercício 1

--- L01Ex01.java
+++ L01Ex01.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class L01Ex01{
+
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int tempoHoras, pacotes;
+        
+        System.out.print("Digite o tempo em horas: ");
+        tempoHoras = input.nextInt();
+        
+        pacotes = (tempoHoras * 3);
+
+        
+        System.out.println("Trafegaram pela rede " + pacotes + " pacotes.");
+
+
+    }
+
+}
+
 
