35ddae9326672b8f146faecb59f7dfe5ccbe85b0
andrerosa98 <202420120000024@pucgo.edu.br>
2024-09-07 14:46:22 -0300

    resolução do exercício 03

--- L01Ex03.java
+++ L01Ex03.java
@@ -5,10 +5,25 @@ public class L01Ex03
 
     public static void main (String[] args)
     {
-
         Scanner input = new Scanner(System.in);
 
+        float teraByte;
+        float gigaByte;
+        float megaByte;
+        float kiloByte;
+        float calcByte;
 
+        System.out.print("Digite um valor em Terabyte: ");
+        teraByte = input.nextFloat();
+
+        gigaByte = (teraByte / 1024.0f);
+        megaByte = (gigaByte / 1024.0f);
+        kiloByte = (megaByte / 1024.0f);
+        calcByte = (kiloByte / 1024.0f);
+
+        System.out.println("Gigabyte: " + gigaByte);
+        System.out.println("Megabyte: " + megaByte);
+        System.out.println("bytes: "  + calcByte);
 
     }
 }
