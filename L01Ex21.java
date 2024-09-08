aab6066c8e3870efd76ae2384bcc195c5dbe3eaa
andrerosa98 <202420120000024@pucgo.edu.br>
2024-09-08 15:57:20 -0300

    resoluçãodoexercício 21

---
+++ L01Ex21.java
@@ -0,0 +1,10 @@
+public class L01Ex21 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("1 - Iniciar Jogo");
+        System.out.println("2 - Restaurar último Jogo");
+        System.out.println("3 - Configurar Jogo");
+        System.out.println("4 - Sair");
+    }    
+}
