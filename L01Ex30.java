320b3da3382ad7cd642138e2d602e67d1c3bb9a1
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-09-10 09:53:12 -0300

    resolução do exercício 30

--- L01Ex30.java
+++ L01Ex30.java
@@ -6,6 +6,18 @@ public class L01Ex30
     {
         Scanner input = new Scanner(System.in);
 
-        
+        float velocidade;
+        float angulo;
+        float alcance;
+        final float gravidade = 9.8
+
+        System.out.print("Digite a velocidade inicial (m/s): ");
+        velocidade = input.nextFloat();
+        System.out.print("Digite o ângulo entre o canhão e o solo: ");
+        angulo = input.nextFloat();
+
+        alcance = ((float)Math.pow(velocidade,2) / gravidade) / Math.sin(angulo);
+
+        System.out.printf("O alcance do projétil será de %.2f metros.", angulo);
     }    
 }
