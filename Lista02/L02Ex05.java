4b1b829e885851a6ee1ee23e7e47330ac9728b2d
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-09-12 12:53:29 -0300

    resolução do exercício 05

--- Lista02/L02Ex05.java
+++ Lista02/L02Ex05.java
@@ -8,17 +8,31 @@ public class L02Ex05{
         int numeroOriginal;
         int numeroReverso;
         
-        System.out.print("Digite um número de 5 dígitos");
+        System.out.print("Digite um número de 5 dígitos: ");
         numero = input.nextInt();
+        numeroOriginal = numero;
 
         numeroReverso = numero % 10;
-        numeroReverso * 10 = numeroReverso;
-        
+        numeroReverso = numeroReverso * 10;
+        numero = numero / 10;
+        numeroReverso = numeroReverso + (numero % 10);
+        numeroReverso = numeroReverso * 10;
+        numero = numero / 10;
+        numeroReverso = numeroReverso + (numero % 10);
+        numeroReverso = numeroReverso * 10;
+        numero = numero / 10;
+        numeroReverso = numeroReverso + (numero % 10);
+        numeroReverso = numeroReverso * 10;
+        numero = numero / 10;
+        numeroReverso = numeroReverso + (numero % 10);
+        numeroReverso = numeroReverso * 10;
+        numero = numero / 10;
+        numeroReverso = numeroReverso / 10;
 
-        if (numero == numeroReverso){
-            System.out.println("É um número palíndromo.");
+        if (numeroOriginal == numeroReverso){
+            System.out.printf("%d É um número palíndromo.", numeroOriginal);
         }else{
-            System.out.println("Não é um número palíndromo.");
+            System.out.printf("%d Não é um número palíndromo.", numeroOriginal);
         }
     }
 }
