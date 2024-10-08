56773a12a9b09d50a78eb99ec4f0f10068ea263f
andrerosa98 <202420120000024@pucgo.edu.br>
2024-10-08 18:08:55 -0300

    início resolução exercício 02

---
+++ Lista03/L03Ex02.java
@@ -0,0 +1,11 @@
+package Lista03;
+
+import java.util.Scanner;
+public class L03Ex02 {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fim");
+    }
+}
