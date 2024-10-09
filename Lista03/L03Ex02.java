a96862852429a877263bbb97e4732a9aa86a2598
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-10-09 14:02:39 -0300

    testar execução exercício 03

--- Lista03/L03Ex02.java
+++ Lista03/L03Ex02.java
@@ -6,6 +6,49 @@ public class L03Ex02 {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Fim");
+        int numConta;
+        String nome;
+        float saldo;
+        int contUsuario = 0;
+        String nomeVip = "a";
+        int contPercentual = 0;
+        float percentUsuario;
+
+        
+        
+        System.out.print("Digite o número da conta: ");
+        numConta = input.nextInt();
+
+        while (numConta >= 0){
+            System.out.print("Digite o seu nome: ");
+            nome = input.next();
+            System.out.print("Digite o saldo: ");
+            saldo = input.nextFloat();
+            contUsuario++;
+
+            if (saldo > 500f){ // Cliente VIP
+                nomeVip = nome;
+            }
+
+            if (saldo >= 100 && saldo <= 200){ //soma do percentual
+                contPercentual++;
+            }
+
+            System.out.print("Digite o número da conta: ");
+            numConta = input.nextInt();
+            input.nextLine();
+
+        }
+
+        percentUsuario = (float) contPercentual / contUsuario; //cálculo percentual
+
+        if (contUsuario != 0){
+            System.out.printf("O percentual de usuários com saldos entre R$100,00 e R$200,00 é: %.2f\n", percentUsuario);
+
+        }
+    
+        if (nomeVip != "a"){
+            System.out.print(nomeVip + "é um cliente VIP");
+        }
     }
 }
