20f6c0f3f3b3f60ee3cf4515fa807135ea4ca0f5
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-11-06 12:52:40 -0300

    cópia do exercício 34 para validação

---
+++ Lista03/L03Ex34.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class L03Ex34{
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+
+        
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 3; j++){
+                for (int k = 1; k <=4; k++)
+                    System.out.print("*");
+                System.out.print("\n");
+            }
+        System.out.print("\n");
+        }
+    }
+}
