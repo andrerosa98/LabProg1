824f19ae54862b87ef8acc9e4e02f95bd5255c39
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-11-23 07:43:29 -0300

    resolução do exercício 07

--- Lista04/L04Ex07.java
+++ Lista04/L04Ex07.java
@@ -6,23 +6,21 @@ public class L04Ex07{
         
         int[] aleatorio = {1,7,3,10,5,9,2,8,6,4};
         int numero;
-        int i = 0;
-        boolean check;
+        int pos = 0;
+        boolean check = false;
 
         System.out.print("Digite um número inteiro positivo: ");
         numero = input.nextInt();
 
-        while (i < aleatorio.length){
+        for (int i = 0; i < aleatorio.length; i++){
             if (aleatorio[i] == numero){
                 check = true;
-            }else{
-                i++;
-                check = false;
+                pos = i;
             }
         }
 
-       if (check = true){
-            System.out.println("O número " + numero + " está localizado na posição " + i + " do vetor.");
+       if (check == true){
+            System.out.println("O número " + numero + " está localizado na posição " + pos + " do vetor.");
         }else{
             System.out.println("O número " + numero + " não está localizado no vetor.");
         }
