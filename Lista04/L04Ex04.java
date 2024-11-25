7f6e02566d42b3b10e8c18068c95c217b1cf9e25
andrerosa98 <202420120000024@pucgo.edu.br>
2024-11-24 21:33:47 -0300

    resolução do exercício 04

--- Lista04/L04Ex04.java
+++ Lista04/L04Ex04.java
@@ -6,6 +6,10 @@ public class L04Ex04 {
         
         char[] vetorFrase = new char[80];
         String frase;
+        int contBranco = 0;
+        int contA = 0;
+        int contDouble = 0;
+        char[] vetorDouble = new char[80];
 
             System.out.println("Digite uma frase de 80 caracteres: ");
             frase = input.nextLine();
@@ -16,8 +20,27 @@ public class L04Ex04 {
                 vetorFrase = frase.toCharArray();
             }
 
-        for (int i = 0; i <vetorFrase.length; i++){
-            System.out.print(vetorFrase[i] + " ");
+        for (int i = 0; i < vetorFrase.length; i++){
+            if (vetorFrase[i] == ' '){
+                contBranco++;
+            }
+            if (vetorFrase[i] == 'a' || vetorFrase[i] == 'A'){
+                contA++;
+            }
+            if (i + 1 < vetorFrase.length && vetorFrase[i] == vetorFrase[i + 1]){
+                contDouble++;
+                vetorDouble[i] = vetorFrase[i];
+            }
+        }
+
+        System.out.println("b) A quantidade de caracteres em branco é: " + contBranco);
+        System.out.println("c) A quantidade de caracteres 'a' ou 'A' é: " + contA);
+        System.out.println("d) A quantidade de caracteres que se repetem é: " + contDouble + " e os caracteres são: ");
+
+        for (int i = 0; i < vetorDouble.length; i++){
+
+        System.out.print(vetorDouble[i] + " ");
+
         }
 
     }
