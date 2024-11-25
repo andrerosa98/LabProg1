a1afb2d1a98bd8b395d2d65d0f86f58d32fb0512
André Luis Ferreira da Rosa Junior <20242012000024@pucgo.edu.br>
2024-11-25 08:11:23 -0300

    nova tentativa de resolução do exercício 09

--- Lista04/L04Ex09.java
+++ Lista04/L04Ex09.java
@@ -8,13 +8,26 @@ public class L04Ex09{
         Random random = new Random();
 
         char[] gabarito = new char[10];
-        int[] numAluno = new int[9999];
+        int[] numAluno = new int[10];
         char[] respostas = new char[(numAluno.length * gabarito.length)];
+        int respostaCerta = 0;
+        int respostaCertaTotal = 0;
+        int respostaErrada = 0;
+        int aprovado = 0;
+        final float APROVACAO = 6.0f;
+        float pctAprovacao;
 
         for (int i = 0; i < gabarito.length; i++){
             System.out.print("Digite o gabarito da pergunta " + (i+1) + ": ");
             char aleatorio = (char) (random.nextInt(4) + 'A');
             gabarito[i] = aleatorio;
+        }
+
+        for (int i = 0; i < numAluno.length; i++){
+           numAluno[i] = random.nextInt(10); 
+        }
+        for (int i = 0; i < (numAluno.length * gabarito.length); i++){
+            respostas[i] = (char) (random.nextInt(4) + 'A');
         }
 
         System.out.println("GABARITO");
@@ -23,5 +36,25 @@ public class L04Ex09{
                                                                  gabarito[5],gabarito[6],gabarito[7],gabarito[8],gabarito[9]);
         System.out.println("---------------------");
 
+        for (int i = 0; i < numAluno.length; i++){
+            for (int j = 0; j < respostas.length; j++){
+                if (respostas[j] == gabarito[j]){
+                    respostaCerta++;
+                    respostaCertaTotal++;
+                }else{
+                    respostaErrada++;
+                }
+            }
+            System.out.println("O aluno de número " + numAluno[i] + " obteve a nota " + respostaCerta++ + " de " + gabarito.length);
+
+            if (respostaCerta > APROVACAO){
+                    aprovado++;
+            }
+            respostaCerta = 0;
+        }
+
+        pctAprovacao = aprovado / numAluno.length;
+
+        System.out.printf("A porcentagem de aprovação foi de %.2f%%\n", pctAprovacao);
     }
 }
